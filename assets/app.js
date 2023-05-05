f5ea9fc8e0b30225ba6a6cf2ed4e8e552853389d
Martin Olmedo <martinolmedo078@gmail.com>
2023-05-05 17:03:43 -0300

    Finalizacion de la modificacion del contenido en la section

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,8 @@
-let botonEncriptar = document.getElementById('encriptar');
-let botonDesencriptar = document.getElementById('desencriptar');
+const botonEncriptar = document.getElementById('encriptar');
+const botonDesencriptar = document.getElementById('desencriptar');
 
 function encriptar(){
-    let textoDesencriptado = document.getElementById('texto').value;
+    const textoDesencriptado = document.getElementById('texto').value;
     let textoEncriptado = "";
     
     for (let index = 0; index < textoDesencriptado.length; index++) {
@@ -25,8 +25,8 @@ function encriptar(){
 }
 
 function desencriptar(){
-    let textoEncriptado = document.getElementById('texto').value;
-    let textoDesencriptado = textoEncriptado
+    const textoEncriptado = document.getElementById('texto').value;
+    const textoDesencriptado = textoEncriptado
         .replaceAll("ai", "a")
         .replaceAll("enter", "e")
         .replaceAll("imes","i")
@@ -35,3 +35,31 @@ function desencriptar(){
     
     return textoDesencriptado;
 }
+
+function modificarContenido(){
+    const imagen = document.getElementById('img-ilustracion');
+    const subtitulo = document.getElementById('subtitulo');
+    const cajaBoton = document.getElementById('caja-boton-copiar');
+    imagen.classList.add('hidden');
+    subtitulo.classList.add('hidden');
+    cajaBoton.classList.remove('hidden');
+}
+
+function mostrarEncriptacion(){
+    const parrafo = document.getElementById('mensaje');
+    if(encriptar() != ""){
+        modificarContenido();
+        parrafo.innerHTML = encriptar();
+    }
+}
+
+function mostrarDesencriptacion(){
+    const parrafo = document.getElementById('mensaje');
+    if(desencriptar() != ""){
+        modificarContenido();
+        parrafo.innerHTML = desencriptar();
+    }
+}
+
+botonEncriptar.addEventListener('click', mostrarEncriptacion);
+botonDesencriptar.addEventListener('click', mostrarDesencriptacion);
